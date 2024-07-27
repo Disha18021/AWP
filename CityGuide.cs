2a5dfca8bc36e79185147c68a6e7cd3ddd535c26
Disha18021 <153249930+Disha18021@users.noreply.github.com>
2024-07-27 13:45:39 +0530

    Add files via upload

--- CityGuide.cs
+++ CityGuide.cs
@@ -1,26 +1,38 @@
-class CityGuide
+class Ex
 {
-	public static void Main()
+	public void Fun( )
 	{
 		System.Console.WriteLine("Select your choice");
 		System.Console.WriteLine("London");
 		System.Console.WriteLine("Bombay");
 		System.Console.WriteLine("Paris");
-		System.Console.WriteLine("Type your choice");
-		String name= Console.ReadLine();
-		
-		switch(name)
+		System.Console.WriteLine("Type-0 your choice");
+		String name = Console.ReadLine( );
+
+		switch (name)
 		{
 			case "Bombay":
-				System.Console.WriteLine("Bombay:Guide 5");
-				break;
+			      Console.WriteLine("Bombay:Guide 5");
+			      break;
 			case "London":
-				System.Console.WriteLine("London:Guide 10");
-				break;
-			case "Parise":
-				System.Console.WriteLine("Paris:Guide 15");
-				break;
-			default:
-				System.Console.WriteLine("Invalid choice");
+			      Console.WriteLine("London:Guide 10");
+			      break;	
+		         case "Bombay":
+			      Console.WriteLine("Paris:Guide 15");
+			      break;	
+			 default:
+			      Console.WriteLine ("Invalid choice");
 		}
+         }
+class CityGuide
+{
+	public static void main(string[] args)
+	{
+		Ex obj=new Ex();
+		obj.Fun();
+	}
 }
+	
+		
+		
+		
